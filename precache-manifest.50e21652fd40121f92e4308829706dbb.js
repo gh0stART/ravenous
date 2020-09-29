f6843f33b63072f118d8790f276fffe1af123acc
gh0stART <69533451+gh0stART@users.noreply.github.com>
2020-09-29 11:29:24 -0700

    Updates

---
+++ precache-manifest.50e21652fd40121f92e4308829706dbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99047aab56529b3f6e45fc35ef8b1dce",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "bfa4822f83e44948ab92",
+    "url": "/ravenous/static/css/main.5038561c.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/ravenous/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa4822f83e44948ab92",
+    "url": "/ravenous/static/js/main.18026040.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "09629d57c465e72a16a89055bd2cc973",
+    "url": "/ravenous/static/media/background_search_desktop.09629d57.jpg"
+  },
+  {
+    "revision": "4bacfdae6bf6358a0c9c653b1cfe9c81",
+    "url": "/ravenous/static/media/background_search_mobile.4bacfdae.jpg"
+  }
+]);
